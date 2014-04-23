67500762a6a122f3250d0c6eb0b2ca70b0ce55fe
Karina <kchan4@wellesley.edu>
2014-04-23 19:48:50 -0400

    print statements

--- classes/SessionVisits.java
+++ classes/SessionVisits.java
@@ -37,7 +37,9 @@ public class SessionVisits extends HttpServlet
 
         Connection con = null;
         try {
+            out.println("try connect");
             con = TraceDB.connect("trace_db"); //need to change this to another one
+            out.println("connectdb");
             pageheader(out,"Visit sessions");
 
             printWelcome(session,out);
@@ -133,6 +135,7 @@ public class SessionVisits extends HttpServlet
     private void printforms(PrintWriter out, Connection con, String self)
         throws SQLException
     {
+        out.println("in printforms")
         Statement query = con.createStatement();
         ResultSet result = query.executeQuery("select students.bid, studname, classes.crn, className from taking, classes, students where students.bid=taking.bid and taking.crn=classes.crn order by classname;");
 
