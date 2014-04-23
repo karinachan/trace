2e3f3ebff8c4da672db49124ed25e451efde0c18
Karina <kchan4@wellesley.edu>
2014-04-22 23:54:47 -0400

    Odd error; trying load data local infile

--- classes/tracCreate.sql
+++ classes/tracCreate.sql
@@ -55,10 +55,10 @@ ENGINE= InnoDB;
 
 
 -- load the .csv files from /tmp/ area...shouldn't be in the /tmp folder anymore because we clean up after ourselves...
-load data infile 'classestable.csv' into table classes fields terminated by ',' lines terminated by '\r';
+load data local infile 'classestable.csv' into table classes fields terminated by ',' lines terminated by '\r';
 -- SET FOREIGN_KEY_CHECKS=0;
 -- previously set this since we were having foreign key constraint issues, fixed by manually creating the first connections
-load data infile 'studentsData.csv' into table students fields terminated by ',' lines terminated by '\r';
+load data local infile 'studentsData.csv' into table students fields terminated by ',' lines terminated by '\r';
 
-load data infile 'takingData.csv' into table taking fields terminated by ',' lines terminated by '\r';
+load data local infile 'takingData.csv' into table taking fields terminated by ',' lines terminated by '\r';
 -- SET FOREIGN_KEY_CHECKS=1;
