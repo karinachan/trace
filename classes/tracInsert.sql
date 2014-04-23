1784baa991775da9e58964bb8739b3dd3d8d8207
Karina <kchan4@wellesley.edu>
2014-04-22 23:44:03 -0400

    Table name typo
    I should really make sure I’m free of errors before committing

--- classes/tracInsert.sql
+++ classes/tracInsert.sql
@@ -3,7 +3,7 @@
 
 
 -- load the .csv files from /tmp/ area...shouldn't be in the /tmp folder anymore because we clean up after ourselves...
-load data infile 'classestable.csv' into table contactID fields terminated by ',' lines terminated by '\r';
+load data infile 'classestable.csv' into table classes fields terminated by ',' lines terminated by '\r';
 -- SET FOREIGN_KEY_CHECKS=0;
 -- previously set this since we were having foreign key constraint issues, fixed by manually creating the first connections
 load data infile 'studentsData.csv' into table students fields terminated by ',' lines terminated by '\r';
