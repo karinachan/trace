52ebdcb92872905f53170cbd6a69727f56ddf977
Karina <kchan4@wellesley.edu>
2014-04-22 21:28:32 -0400

    Typo on comment syntax :|

--- classes/tracInsert.sql
+++ classes/tracInsert.sql
@@ -1,5 +1,5 @@
 
---goal of this file is to create a csv so that we can easily add information to manipulate
+-- goal of this file is to create a csv so that we can easily add information to manipulate
 
 
 -- load the .csv files from /tmp/ area...shouldn't be in the /tmp folder anymore because we clean up after ourselves...
