ea1e4e19864cf879ee37f9eff77f65bb5c457812
Karina <kchan4@wellesley.edu>
2014-04-23 21:49:25 -0400

    ORDER 4

--- classes/tracCreate.sql
+++ classes/tracCreate.sql
@@ -5,12 +5,12 @@
 -- remove existing tables
 
 drop table if exists sessions;
+
+drop table if exists visting;
+drop table if exists taking;
 drop table if exists tutors;
 drop table if exists students;
 drop table if exists classes;
-drop table if exists visting;
-drop table if exists taking;
-
 
 
 
